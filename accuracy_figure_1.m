ba9caf2c48608fc279715261080b1364b341d471
Adrian Radillo <adrian.radillo@gmail.com>
2018-10-27 12:46:43 -0400

    re-run accuracies

--- accuracy_figure_1.m
+++ accuracy_figure_1.m
@@ -1,6 +1,8 @@
 % produce accuracy figure described here
 % https://paper.dropbox.com/doc/Figure-4-design--APE0_g8NTsvVZxTg2DUyd6WvAg-nlH9WGli5QW8x41ZJbGlJ#:h2=Accuracy-as-function-of-discou
 clear
+tbUseProject('analysis_and_fits_dyn_clicks');
+data_folder='~/programing/data/clicks/';
 tic
 % noise levels
 kappa=log(20/5);
@@ -8,16 +10,16 @@ noise_vec=[.1,kappa,2*kappa];
 
 % discounting parameters to probe
 disc.lin=0:.5:10;
-disc.nonlin=0:.1:4;
+disc.nonlin=[0:.01:.09,.1:.1:1];
 
 % get accuracy values
-dbname='~/programing/data/clicks/db1.h5';
+dbname=[data_folder,'db1.h5'];
 trial_range=[1,10000];
 % what I call a combination is a pair (model_type,noise level)
 num_combinations = 2*length(noise_vec); % first 3 for L; last 3 for NL
 acc_store=cell(1,num_combinations);
 for s=1:num_combinations
-    if s<4; 
+    if s<4
         disc_vec=disc.lin; 
         model_type='lin';
     else
@@ -35,23 +37,23 @@ for s=1:num_combinations
     end
     acc_store{s}=curve;
 end
-
-% panel A for linear model
-subplot(1,2,1)
-plot(disc.lin,acc_store{1})
-hold on
-plot(disc.lin,acc_store{2})
-plot(disc.lin,acc_store{3})
-hold off
-title('L')
-legend(num2str(noise_vec(1)),num2str(noise_vec(2)),num2str(noise_vec(3)))
-% panel B for nonlinear model
-subplot(1,2,2)
-plot(disc.nonlin,acc_store{4})
-hold on
-plot(disc.nonlin,acc_store{5})
-plot(disc.nonlin,acc_store{6})
-hold off
-title('NL')
-legend(num2str(noise_vec(1)),num2str(noise_vec(2)),num2str(noise_vec(3)))
 toc
+save([data_folder,'accuracy_figure_1.mat']);
+% panel A for linear model
+% subplot(1,2,1)
+% plot(disc.lin,acc_store{1})
+% hold on
+% plot(disc.lin,acc_store{2})
+% plot(disc.lin,acc_store{3})
+% hold off
+% title('L')
+% legend(num2str(noise_vec(1)),num2str(noise_vec(2)),num2str(noise_vec(3)))
+% % panel B for nonlinear model
+% subplot(1,2,2)
+% plot(disc.nonlin,acc_store{4})
+% hold on
+% plot(disc.nonlin,acc_store{5})
+% plot(disc.nonlin,acc_store{6})
+% hold off
+% title('NL')
+% legend(num2str(noise_vec(1)),num2str(noise_vec(2)),num2str(noise_vec(3)))
