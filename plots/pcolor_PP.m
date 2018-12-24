43ceff0776766b5531535b6ca598765567705494
Adrian Radillo <adrian.radillo@gmail.com>
2018-12-24 10:22:47 -0500

    pcolor_PP turned into function

--- plots/pcolor_PP.m
+++ plots/pcolor_PP.m
@@ -1,174 +1,148 @@
+function pcolor_PP(model_pair, data_folder, data_file_name, noiseVal, saveFlag)
 % produce heatmaps for predictive power
-%% L-L
-clear
-data_folder='~/programing/data/clicks/';
+% ARGS:
+%   model_pair: 'L-L' or 'NL-NL'
+%   data_folder: e.g. '~/programing/data/clicks/'
+%   data_file_name: e.g.
+%       'joint_PP_LL_ntrials_999990_noise_0.1.mat
+%       'joint_PP_LL_ntrials_999990_noise_1.mat'
+%       'joint_PP_LL_ntrials_999990_noise_2.mat'
+%       'joint_PP_NLNL_ntrials_999990_noise_0.1.mat'
+%       'joint_PP_NLNL_ntrials_999990_noise_1.mat'
+%       'joint_PP_NLNL_ntrials_999990_noise_2.mat'
+%   noiseVal: e.g. 0.1 or 1 or 2  % used for plot label
+%   saveFlag: true or false % if true, saves figures to files
+
 fs=20; % font size
 lw=3; % linewidth
 
-%load([data_folder,'joint_PP_LL_ntrials_999990_noise_0.1.mat'])
-load([data_folder,'joint_PP_LL_ntrials_999990_noise_1.mat'])
-%load([data_folder,'joint_PP_LL_ntrials_999990_noise_2.mat'])
+SS = load([data_folder,data_file_name]);
+
+[X,Y]=meshgrid(SS.thetas_1,SS.thetas_2);
+num_x=length(SS.thetas_1);
+
+[A,B]=max(SS.PP);
+th1max=zeros(size(B));
+for i=1:length(B)
+    th1max(i)=SS.thetas_1(B(i));
+end
+
+% set the upper-left part of the domain to 1 so that it gets plotted in
+% white in the colormap
+%for i=1:num_g
+%    for j=1:i-1
+%        PP(i,j)=1;
+%    end
+%end
+
+% set cutoff values for colormap
+%nonZeros = find(PP);
+absMax = 1;% max(max(PP));
+absMin = .5; %min(min(PP(nonZeros)));
+
+% ------------- run once with colorbar -------------------%
+fig1 = figure(1); 
+ax1=gca; 
+pc=pcolor(ax1,X,Y,SS.PP);
+
+% remove grid overlayed on plot
+set(pc,'EdgeColor','None')
+
+% set the interval of values outside of which the colormap is constant
+caxis([absMin,absMax])
+
+% add colorbar
+hhh = colorbar('XTick',[.5,1]);
+ylabel(hhh,'PP')
+ax1.FontSize = fs;
+
+% set the colormap for the heatmap
+cmap = colormap('copper');
+%cmap(end,:) = [1,1,1];
+colormap(cmap)
+
+% ------- run a second time without colorbar -------------%
+
+fig2 = figure(2); 
+ax1=gca;
+pc=pcolor(ax1,X,Y,SS.PP);
+
+% remove grid overlayed on plot
+set(pc,'EdgeColor','None')
+
+% set the interval of values outside of which the colormap is constant
+caxis([absMin,absMax])
+
+% add colorbar
+% hhh = colorbar('XTick',[.5,1]);
+% ylabel(hhh,'PP')
+
+% set the colormap for the heatmap
+cmap = colormap('copper');
+%cmap(end,:) = [1,1,1];
+colormap(cmap)
+
+hold on
+
+% plot ridge
+plot(ax1,SS.thetas_1,th1max,'LineWidth',lw,'Color','red')
+%plot(ax1,th1max,thetas_1,'LineWidth',lw,'Color','red')
+
+% plot diagonal
+plot(SS.thetas_1,SS.thetas_1,'--r','LineWidth',lw-1)
+hold off
+
+% set y-axis on the right
+set(ax1,'YAxisLocation','right')
+title([model_pair,' noise=',num2str(noiseVal)])
+if strcmp(model_pair,'L-L')
+    xlabel('\gamma_1')
+    ax1.XAxis.TickValues=0:5:10;
+    ylabel('\gamma_2')
+    ax1.YAxis.TickValues = 0:5:10;
+elseif strcmp(model_pair,'NL-NL')
+    xlabel('h_1')
+    ax1.XAxis.TickValues=[0,2.5];
+    ylabel('h_2')
+    ax1.YAxis.TickValues = [0,2.5];
+end
+ax1.FontSize=fs;
+
+% --------------------------------------------%
 
 
-[X,Y]=meshgrid(thetas_1,thetas_2);
-num_g=length(thetas_1);
-% 
-% for i=1:num_g
+% for i=1:num_x
 %     for j=1:i-1
 %         PP(i,j)=PP(j,i);
 %     end
 % end
 
+% 
+% [A,B]=max(PP);
+% th1max=zeros(size(B));
+% for i=1:length(B)
+%     th1max(i)=thetas_1(B(i));
+% end
 
-[~,B]=max(PP);
-th1max=zeros(size(B));
-for i=1:length(B)
-    th1max(i)=thetas_1(B(i));
+% produce inset
+fig3 = figure(3);
+plot(SS.thetas_1,A, '-r', 'LineWidth',lw)
+hold on 
+plot(SS.thetas_1, diag(SS.PP), '--r', 'LineWidth', lw)
+hold off
+ylabel('PP')
+ylim([absMin, absMax])
+yticks([.5,.8,.9,1])
+if strcmp(model_pair,'L-L')
+    xlim([0,10])
+    xticks([0,10])
+elseif strcmp(model_pair,'NL-NL')
+    xlim([0,2.5])
+    xticks([0,2.5])
 end
-nonZeros = find(PP);
-absMax = max(max(PP));
-absMin = min(min(PP(nonZeros)));
 
-% figure()
-% subplot(2,1,1)
-% hold on
-% plot(ax1,thetas_1,th1max,'LineWidth',lw)
-% plot(ax1,th1max,thetas_1,'LineWidth',lw)
-% plot(thetas_1,thetas_1,'--r','LineWidth',lw-1)
-% hold off
-% title('max PP - LL - 1M trials')
-% xlabel('\theta_2')
-% ylabel('\theta_1^{max}')
-% ax=gca; ax.FontSize=fs;
-% 
-% subplot(2,1,2)
-% plot(thetas_1,A)
-% ylabel('max PP')
+legend('max','diag','Location','southeast')
+ax=gca; ax.FontSize=fs;
+% ------ save to file
 
-
-
-figure(1)
-ax1=gca;
-pc=pcolor(ax1,X,Y,PP);
-colorbar
-set(pc,'EdgeColor','None')
-colormap('copper')
-caxis([absMin,absMax])
-hold on
-plot(ax1,thetas_1,th1max,'LineWidth',lw,'Color','red')
-%plot(ax1,th1max,thetas_1,'LineWidth',lw,'Color','red')
-plot(thetas_1,thetas_1,'--r','LineWidth',lw-1)
-hold off
-title('Predictive Power L-L')
-xlabel('\gamma_1')
-ylabel('\gamma_2')
-hold off
-ax1.FontSize=fs;
-
-%figure()
-
-
-
-% figure()
-% for ii=1:3
-%     ax=subplot(3,1,ii);
-%     n=ii*floor(num_g/3);
-%     plot(thetas_1,PP(n,:),'LineWidth',lw)
-%     [~,mx]=max(PP(n,:));
-%     hold on
-%     plot([thetas_1(n),thetas_1(n)],[ax.YLim(1),ax.YLim(2)],'--r',...
-%         'LineWidth',lw)
-%     plot([thetas_1(mx),thetas_1(mx)],[ax.YLim(1),ax.YLim(2)],'-k',...
-%         'LineWidth',lw-1)
-%     hold off
-%     title(['1M trials - ref \theta_1 = ',num2str(thetas_1(n))])
-%     xlabel('\theta_2')
-%     ylabel('PP')
-%     legend('PP','\theta_1=\theta_2','max')
-% end
-
-
-%% NL-NL
-clear
-data_folder='~/programing/data/clicks/';
-fs=20; % font size
-lw=3; % linewidth
-
-
-%load([data_folder,'joint_PP_NLNL_ntrials_999990_noise_0.1.mat'])
-load([data_folder,'joint_PP_NLNL_ntrials_999990_noise_1.mat'])
-%load([data_folder,'joint_PP_NLNL_ntrials_999990_noise_2.mat'])
-
-[X,Y]=meshgrid(thetas_1,thetas_2);
-num_h=length(thetas_1);
-% for i=1:num_h
-%     for j=1:i-1
-%         PP(i,j)=PP(j,i);
-%     end
-% end
-
-[A,B]=max(PP);
-th1max=zeros(size(B));
-for i=1:length(B)
-    th1max(i)=thetas_1(B(i));
 end
-nonZeros = find(PP);
-absMax = max(max(PP));
-absMin = min(min(PP(nonZeros)));
-% figure()
-% subplot(2,1,1)
-% plot(thetas_1,h1max,'LineWidth',lw)
-% hold on
-% plot(thetas_1,thetas_1,'--r','LineWidth',lw-1)
-% hold off
-% 
-% title('max PP - NLNL')
-% xlabel('\theta_2')
-% ylabel('\theta_1^{max}')
-% ax=gca; ax.FontSize=fs;
-% 
-% subplot(2,1,2)
-% plot(thetas_1,A)
-% ylabel('max PP')
-
-
-figure()
-ax1=gca;
-pc=pcolor(ax1,X,Y,PP);
-set(pc,'EdgeColor','None')
-%shading flat
-colormap('copper')
-caxis([absMin,absMax])
-colorbar
-hold on
-plot(ax1,thetas_1,th1max,'LineWidth',lw,'Color','red')
-%plot(ax1,th1max,thetas_1,'LineWidth',lw,'Color','red')
-plot(thetas_1,thetas_1,'--r','LineWidth',lw-1)
-hold off
-title('Predictive Power NL-NL')
-xlabel('h_1')
-ylabel('h_2')
-hold off
-ax1.FontSize=fs;
-
-
-
-
-% figure()
-% for ii=1:3
-%     ax=subplot(3,1,ii);
-%     n=ii*floor(num_h/3);
-%     plot(thetas_1,PP(n,:),'LineWidth',lw)
-%     [~,mx]=max(PP(n,:));
-%     hold on
-%     plot([thetas_1(n),thetas_1(n)],[ax.YLim(1),ax.YLim(2)],'--r',...
-%         'LineWidth',lw)
-%     plot([thetas_1(mx),thetas_1(mx)],[ax.YLim(1),ax.YLim(2)],'-k',...
-%         'LineWidth',lw-1)
-%     hold off
-%     title(['ref \theta_1 = ',num2str(thetas_1(n))])
-%     xlabel('\theta_2')
-%     ylabel('PP')
-%     legend('PP','\theta_1=\theta_2','max')
-%     ax.FontSize=fs;
-% end
