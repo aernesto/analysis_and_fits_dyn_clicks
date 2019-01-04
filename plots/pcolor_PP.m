0dc31b8c506888faac1b8de4b6fa223b18c8114d
Adrian Radillo <adrian.radillo@gmail.com>
2019-01-04 15:29:06 -0500

    produce plots for figure 5 (heatmaps)

--- plots/pcolor_PP.m
+++ plots/pcolor_PP.m
@@ -12,14 +12,20 @@ function pcolor_PP(model_pair, data_folder, data_file_name, noiseVal, saveFlag)
 %       'joint_PP_NLNL_ntrials_999990_noise_2.mat'
 %   noiseVal: e.g. 0.1 or 1 or 2  % used for plot label
 %   saveFlag: true or false % if true, saves figures to files
+% RETURNS:
+%   nothing, but creates plots and saves them to file if saveFlag is true
 
 fs=20; % font size
 lw=3; % linewidth
+inset_factor=.25;
+font_factor = 1/1.5;
+inset_lw = lw;% / inset_factor;
+inset_fs = font_factor * fs;% / inset_factor;
 
 SS = load([data_folder,data_file_name]);
 
 [X,Y]=meshgrid(SS.thetas_1,SS.thetas_2);
-num_x=length(SS.thetas_1);
+
 
 [A,B]=max(SS.PP);
 th1max=zeros(size(B));
@@ -29,19 +35,20 @@ end
 
 % set the upper-left part of the domain to 1 so that it gets plotted in
 % white in the colormap
-%for i=1:num_g
+% num_x=length(SS.thetas_1);
+% for i=1:num_x
 %    for j=1:i-1
-%        PP(i,j)=1;
+%        SS.PP(i,j)=1;
 %    end
-%end
+% end
 
 % set cutoff values for colormap
 %nonZeros = find(PP);
 absMax = 1;% max(max(PP));
 absMin = .5; %min(min(PP(nonZeros)));
 
-% ------------- run once with colorbar -------------------%
-fig1 = figure(1); 
+%% ------------- run once with colorbar -------------------%
+fig1 = figure(); 
 ax1=gca; 
 pc=pcolor(ax1,X,Y,SS.PP);
 
@@ -61,9 +68,9 @@ cmap = colormap('copper');
 %cmap(end,:) = [1,1,1];
 colormap(cmap)
 
-% ------- run a second time without colorbar -------------%
+%% ------- run a second time without colorbar -------------%
 
-fig2 = figure(2); 
+fig2 = figure(); 
 ax1=gca;
 pc=pcolor(ax1,X,Y,SS.PP);
 
@@ -74,8 +81,8 @@ set(pc,'EdgeColor','None')
 caxis([absMin,absMax])
 
 % add colorbar
-% hhh = colorbar('XTick',[.5,1]);
-% ylabel(hhh,'PP')
+%hhh = colorbar('XTick',[.5,1]);
+%ylabel(hhh,'PP')
 
 % set the colormap for the heatmap
 cmap = colormap('copper');
@@ -85,11 +92,11 @@ colormap(cmap)
 hold on
 
 % plot ridge
-plot(ax1,SS.thetas_1,th1max,'LineWidth',lw,'Color','red')
+ridge_curve = plot(ax1,SS.thetas_1,th1max,'LineWidth',lw,'Color','red');
 %plot(ax1,th1max,thetas_1,'LineWidth',lw,'Color','red')
 
 % plot diagonal
-plot(SS.thetas_1,SS.thetas_1,'--r','LineWidth',lw-1)
+diag_curve = plot(SS.thetas_1,SS.thetas_1,'--r','LineWidth',lw-1);
 hold off
 
 % set y-axis on the right
@@ -107,32 +114,22 @@ elseif strcmp(model_pair,'NL-NL')
     ax1.YAxis.TickValues = [0,2.5];
 end
 ax1.FontSize=fs;
+legend([ridge_curve, diag_curve],{'max','diag'},'Location','west',...
+    'FontSize', inset_fs)
 
-% --------------------------------------------%
 
-
-% for i=1:num_x
-%     for j=1:i-1
-%         PP(i,j)=PP(j,i);
-%     end
-% end
-
-% 
-% [A,B]=max(PP);
-% th1max=zeros(size(B));
-% for i=1:length(B)
-%     th1max(i)=thetas_1(B(i));
-% end
+%% --------------------------------------------%
 
 % produce inset
-fig3 = figure(3);
-plot(SS.thetas_1,A, '-r', 'LineWidth',lw)
+fig3 = figure();
+
+plot(SS.thetas_1,A, '-r', 'LineWidth',inset_lw)
 hold on 
-plot(SS.thetas_1, diag(SS.PP), '--r', 'LineWidth', lw)
+plot(SS.thetas_1, diag(SS.PP), '--r', 'LineWidth', inset_lw)
 hold off
 ylabel('PP')
 ylim([absMin, absMax])
-yticks([.5,.8,.9,1])
+yticks([.5,.8,1])
 if strcmp(model_pair,'L-L')
     xlim([0,10])
     xticks([0,10])
@@ -141,10 +138,10 @@ elseif strcmp(model_pair,'NL-NL')
     xticks([0,2.5])
 end
 
-legend('max','diag','Location','southeast')
-ax=gca; ax.FontSize=fs;
+ax=gca; ax.FontSize=inset_fs;
+ax.Position = inset_factor * ones(1,4);
 
-% ------ save to file
+%% ------ save to file
 if saveFlag
     figNum = '1';
     fileNameForSave = ['fig',figNum,model_pair,num2str(noiseVal),'.png'];
--- plots/plot_pcolor_script.m
+++ plots/plot_pcolor_script.m
@@ -1,18 +1,19 @@
 clear
+% run following line in console if project not yet activated by TbTb
+% tbUseProject('analysis_and_fits_dyn_clicks');
 modelPairs = {'L-L','NL-NL'};
-noiseVals = [.1,1,2];
+noiseVals = [.1,2];
+tot_noise = length(noiseVals);
 filenames={'joint_PP_LL_ntrials_999990_noise_0.1.mat',...
-      'joint_PP_LL_ntrials_999990_noise_1.mat',...
       'joint_PP_LL_ntrials_999990_noise_2.mat',...
       'joint_PP_NLNL_ntrials_999990_noise_0.1.mat',...
-      'joint_PP_NLNL_ntrials_999990_noise_1.mat',...
       'joint_PP_NLNL_ntrials_999990_noise_2.mat'};
 data_folder='~/programing/data/clicks/';
 for mp = 1:2
     model_pair = modelPairs{mp};
-    for nv = 1:3
+    for nv = 1:length(noiseVals)
         noiseVal = noiseVals(nv);
-        iii = (mp-1)*3+nv;
+        iii = (mp-1)*tot_noise+nv;
         fileName = filenames{iii};
         pcolor_PP(model_pair, data_folder, fileName, noiseVal, true)
     end
